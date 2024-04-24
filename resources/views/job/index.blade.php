d313ef6a31de7c6dc3c018ac1c5a5fcce5d11ed3
Jayza187 <brunopenner01@gmail.com>
2024-04-24 08:01:01 +0800

    #112 Form search

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,21 +3,25 @@
     :links="['Jobs' => route('jobs.index')]" />
 
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div class="">
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input name="search" value="" placeholder="Search for any text" />
-            </div>
-            <div class="">
-                <div class="mb-1 font-semibold">Salary</div>
-                <div class="flex space-x-2">
-                    <x-text-input name="min_salary" value="" placeholder="From" />
-                    <x-text-input name="max_salary" value="" placeholder="To" />
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div class="">
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name="search" value="" placeholder="Search for any text" />
                 </div>
+                <div class="">
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="" placeholder="From" />
+                        <x-text-input name="max_salary" value="" placeholder="To" />
+                    </div>
+                </div>
+                <div class="">3</div>
+                <div class="">4</div>
             </div>
-            <div class="">3</div>
-            <div class="">4</div>
-        </div>
+
+            <button class="w-full">Filter</button>
+        </form>
     </x-card>
 
     @foreach ($jobs as $job)
