caefc6cbdb31dff2a96b6a1cce53c3f790c8b78d
Jayza187 <brunopenner01@gmail.com>
2024-05-02 10:00:04 +0800

    #116 Filtering

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -5,10 +5,10 @@
         <span class="ml-2">All</span>
     </label>
 
-    @foreach ($options as $option)
+    @foreach ($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
             <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request($name))/>
-            <span class="ml-2 capitalize">{{ $option }}</span>
+            <span class="ml-2 capitalize">{{ $label }}</span>
         </label>
     @endforeach
 </div>
