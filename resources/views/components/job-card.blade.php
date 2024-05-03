40d04a712f8eeb49d7432bd22d83c46221107465
Jayza187 <brunopenner01@gmail.com>
2024-05-03 10:09:58 +0800

    #118 gradient bg and styling filter button

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -12,8 +12,12 @@
             <div>{{$job->location}}</div>
         </div>
         <div class="flex space-x-1 text-s">
-            <x-tag>{{Str::ucfirst($job->experience)}}</x-tag>
-            <x-tag>{{$job->category}}</x-tag>
+            <a href="{{ route('jobs.index', ['experience' => $job->experience]) }}">
+                <x-tag>{{Str::ucfirst($job->experience)}}</x-tag>
+            </a>
+            <a href="{{ route('jobs.index', ['category' => $job->category]) }}"> 
+                <x-tag>{{$job->category}}</x-tag> 
+            </a>
         </div>
     </div>
 
