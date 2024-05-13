53267b40f6a0f25ab4747cc67bc9d22dea96b43e
Jayza187 <brunopenner01@gmail.com>
2024-05-13 09:53:55 +0800

    #124 Related Jobs -> other jobs from same company

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -6,4 +6,24 @@
             {!! nl2br(e($job->description)) !!}
         </p>
     </x-job-card>
+
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium">
+            More {{$job->employer->company_name}} Jobs
+        </h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs as $otherJob)
+            <a href="{{ route('jobs.show', $otherJob) }}">
+                <div class="mb-4 flex justify-between">
+                    <div class="text-slate-700">
+                        {{$otherJob->title}} - 
+                        <span class="text-slate-500 text-xs">{{$otherJob->created_at->diffForHumans()}}</span>
+                    </div>
+                    <div class="text-xs">${{number_format($otherJob->salary)}}</div>
+                </div>
+            </a>
+            @endforeach
+        </div>
+    </x-card>
 </x-layout>
